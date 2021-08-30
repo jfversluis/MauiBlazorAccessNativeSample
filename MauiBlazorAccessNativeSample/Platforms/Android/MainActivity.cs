cef45d229e875f4188ac44fd85d596729c9d39f0
Gerald Versluis <github@geraldversluis.nl>
2021-08-30 10:28:26 +0200

    Access Location

--- MauiBlazorAccessNativeSample/Platforms/Android/MainActivity.cs
+++ MauiBlazorAccessNativeSample/Platforms/Android/MainActivity.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 using Microsoft.Maui;
 
 namespace MauiBlazorAccessNativeSample
@@ -7,6 +9,18 @@ namespace MauiBlazorAccessNativeSample
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
-        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Microsoft.Maui.Essentials.Platform.Init(this, savedInstanceState);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Microsoft.Maui.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 }
